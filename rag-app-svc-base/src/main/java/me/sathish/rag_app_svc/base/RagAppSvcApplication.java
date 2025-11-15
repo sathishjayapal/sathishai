8d83e9a2c9b1a607bfc818f9b0f6898153e58172
Bootify <>
2025-11-15 18:20:23 +0000

    update actuator endpoints

--- rag-app-svc-base/src/main/java/me/sathish/rag_app_svc/base/RagAppSvcApplication.java
+++ rag-app-svc-base/src/main/java/me/sathish/rag_app_svc/base/RagAppSvcApplication.java
@@ -3,12 +3,10 @@ package me.sathish.rag_app_svc.base;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 
 @SpringBootApplication
 @ComponentScan("me.sathish.rag_app_svc")
-@EnableAsync
 public class RagAppSvcApplication {
 
     public static void main(final String[] args) {
