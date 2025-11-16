c199baa16c5dcffa12eeecd768a883df109abacf
Sathish Jayapal <sjayapal@skminfotech.com>
2025-11-16 14:01:54 -0600

    refactor: Disable default Spring JPA and DataSource auto-configuration

--- rag-app-svc-base/src/main/java/me/sathish/rag_app_svc/base/RagAppSvcApplication.java
+++ rag-app-svc-base/src/main/java/me/sathish/rag_app_svc/base/RagAppSvcApplication.java
@@ -2,11 +2,16 @@ package me.sathish.rag_app_svc.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 
-@SpringBootApplication
+@SpringBootApplication (exclude = {
+DataSourceAutoConfiguration .class,
+        HibernateJpaAutoConfiguration.class
+    })
 @ComponentScan("me.sathish.rag_app_svc")
 @EnableAsync
 public class RagAppSvcApplication {
--- rag-app-svc-base/src/main/java/me/sathish/rag_app_svc/base/config/DomainConfig.java
+++ rag-app-svc-base/src/main/java/me/sathish/rag_app_svc/base/config/DomainConfig.java
@@ -1,14 +1,8 @@
 package me.sathish.rag_app_svc.base.config;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @Configuration
-@EntityScan("me.sathish.rag_app_svc.base")
-@EnableJpaRepositories("me.sathish.rag_app_svc.base")
-@EnableTransactionManagement
 public class DomainConfig {
 }
